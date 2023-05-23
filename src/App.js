0928b630e08a9ee857836afbf7bb6a8458cd5c29
CMLTTN <cemaldim@gmail.com>
2023-05-23 21:25:40 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+ import "./app.scss";
+ import Header from "./components/Header/Header";
+ import Main from "./components/Main/Main";
+ import Navbar from "./components/Navbar/Navbar";
+
+
+
+ const App = ()=>{
+
+  return(
+    <div className="App" >
+    <Navbar/>
+    <Header/>
+    <Main/>
+      
+    </div>
+  )
+ }
+ export default App
---
+++ src/helpers/data.js
@@ -0,0 +1,6 @@
+@import "variables", "reset";
+body {
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+    text-align: center;
+}
