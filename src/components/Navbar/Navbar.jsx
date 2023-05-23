a4a5ce9e0b7169c1a1c77bf8647f1e235cd090ac
CMLTTN <cemaldim@gmail.com>
2023-05-23 22:08:55 +0300

    all

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import "./Navbar.scss";
+import logo from "../../helpers/logo .png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar" >
+    <div className="logo" >
+      <img src={logo}  alt="" />
+    </div>
+    <div className="links" >
+    <a href="#horoscope">Horoscope</a>
+    <a href="#daily">Daily</a>
+    <a href="#tarot">Tarot</a>
+    <a href="#article">Article</a>
+    <a href="#contact">Contact</a>
+
+    </div>
+    
+    </div>
   )
 }
 
